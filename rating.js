f3cbd822d6303db44b7865f5da677f33e59b3b16
Karen <karenjamalyan@gmail.com>
2025-10-18 17:24:22 +0300

    log index

--- rating.js
+++ rating.js
@@ -281,7 +281,7 @@
 						if (imdb_id) {
 							$.get(`http://www.omdbapi.com/?i=${imdb_id}&apikey=2a9eadde`, function(data) {
 								if (data.imdbRating && data.imdbRating !== 'N/A') {
-									console.log('event.items: ', event.items);
+									console.log('event.items: ', event.items, 'index: ', index);
 									var cardInstance = event.items[index];
                 					var cardElement = cardInstance.render(); 
 									$('.card__vote', cardElement).text($('.card__vote', cardElement).text() + ' IMDb: ' + data.imdbRating);
